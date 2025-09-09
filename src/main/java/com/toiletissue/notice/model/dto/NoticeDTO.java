a742f6849b0cef2df53eb9036ac444371045c54f
I7E-65\i7E-63 <rjsn4433@gmail.com>
2025-09-09 17:51:38 +0900

    [feature] 관리자 공지사항 추가 기능 구현
    공지사항 제목, 내용 받아서 추가하는 기능 구현

--- src/main/java/com/toiletissue/notice/model/dto/NoticeDTO.java
+++ src/main/java/com/toiletissue/notice/model/dto/NoticeDTO.java
@@ -1,4 +1,62 @@
 package com.toiletissue.notice.model.dto;
 
+import java.util.Date;
+
 public class NoticeDTO {
+
+    private int no;
+    private String title;
+    private String content;
+    private Date date;
+
+    public NoticeDTO(){}
+
+    public NoticeDTO(int no, String title, String content, Date date) {
+        this.no = no;
+        this.title = title;
+        this.content = content;
+        this.date = date;
+    }
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "NoticeDTO{" +
+                "no=" + no +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", date=" + date +
+                '}';
+    }
 }
