f033ad790b21f7ce2d8b7cda06537f5beb93fdb2
pcl9556 <pcl9556@naver.com>
2025-09-10 16:26:16 +0900

    [feature] 회원가입 페이지 구현
    auth 패키지 작성

--- src/main/java/com/toiletissue/auth/model/service/AuthService.java
+++ src/main/java/com/toiletissue/auth/model/service/AuthService.java
@@ -1,4 +1,31 @@
 package com.toiletissue.auth.model.service;
 
-public class AuthService {
+import com.toiletissue.auth.model.AuthDetails;
+import com.toiletissue.member.model.dto.MemberLoginDTO;
+import com.toiletissue.member.model.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+public class AuthService implements UserDetailsService {
+
+    @Autowired
+    private MemberService memberService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        MemberLoginDTO login = memberService.findByUsername(username);
+
+        if(Objects.isNull(login)) {
+            throw new UsernameNotFoundException("해당하는 회원 정보가 존재하지 않습니다.");
+        }
+
+        return new AuthDetails(login);
+    }
 }
