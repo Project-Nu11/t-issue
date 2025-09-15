6d672c288dbbeb1056b17aedd75f422d88c1b7fb
I7E-72\i7E-63 <ces234021@gmail.com>
2025-09-15 15:19:43 +0900

    [feature] 화장실 조회 api 연동
    사용자가 화장실 조회 페이지 api를 연동하였다

--- src/main/java/com/toiletissue/member/controller/MemberController.java
+++ src/main/java/com/toiletissue/member/controller/MemberController.java
@@ -1,49 +1,48 @@
-package com.toiletissue.member.controller;
-
-
-import com.toiletissue.member.model.dto.MemberSignupDTO;
-import com.toiletissue.member.model.service.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-@Controller
-@RequestMapping("/member")
-public class MemberController {
-
-    @Autowired
-    private MemberService memberService;
-
-    @GetMapping("/manager")
-    public void reviewManager(){}
-
-    @GetMapping("/signup")
-    public void signup() {
-    }
-
-    @PostMapping("/signup")
-    public String signup(MemberSignupDTO memberSignupDTO, RedirectAttributes rttr) {
-
-        int result = memberService.insert(memberSignupDTO);
-
-        if (result > 0) {
-            rttr.addFlashAttribute("message", "회원가입이 정상적으로 완료되었습니다.");
-//            return "redirect:/auth/login";// 로그인 페이지 리다이렉트
-            return "main";
-
-        } else {
-            rttr.addFlashAttribute("message", "회원가입에 실패하였습니다.");
-            return "redirect:/member/signup";       // 다시 회원가입 화면
-        }
-    }
-
-
-    @GetMapping("/check-id")
-    @ResponseBody
-    public String checkId(@RequestParam String memberId) {
-        return memberService.exists(memberId) ? "이미 존재하는 아이디입니다." : "사용 가능한 아이디입니다.";  //프론트
-    }
-}
-
-
+//package com.toiletissue.member.controller;
+//
+//
+//import com.toiletissue.member.model.dto.MemberSignupDTO;
+//import com.toiletissue.member.model.service.MemberService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.*;
+//import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+//
+//@Controller
+//@RequestMapping("/member")
+//public class MemberController {
+//
+//    @Autowired
+//    private MemberService memberService;
+//
+//    @GetMapping("/manager")
+//    public void reviewManager(){}
+//
+//    @GetMapping("/signup")
+//    public void signup() {
+//    }
+//
+//    @PostMapping("/signup")
+//    public String signup(MemberSignupDTO memberSignupDTO, RedirectAttributes rttr) {
+//
+//        int result = memberService.insert(memberSignupDTO);
+//
+//        if (result > 0) {
+//            rttr.addFlashAttribute("message", "회원가입이 정상적으로 완료되었습니다.");
+////            return "redirect:/auth/login";// 로그인 페이지 리다이렉트
+//            return "main";
+//
+//        } else {
+//            rttr.addFlashAttribute("message", "회원가입에 실패하였습니다.");
+//            return "redirect:/member/signup";       // 다시 회원가입 화면
+//        }
+//    }
+//
+//
+//    @GetMapping("/check-id")
+//    @ResponseBody
+//    public String checkId(@RequestParam String memberId) {
+//        return memberService.exists(memberId) ? "이미 존재하는 아이디입니다." : "사용 가능한 아이디입니다.";  //프론트
+//    }
+//}
+//
--- src/main/java/com/toiletissue/toilet/controller/ToiletController.java
+++ src/main/java/com/toiletissue/toilet/controller/ToiletController.java
@@ -1,9 +1,18 @@
 package com.toiletissue.toilet.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.toiletissue.toilet.model.dto.ToiletDTO;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/toilet")
@@ -12,21 +21,61 @@ public class ToiletController {
     @GetMapping("/manager")
     public void toiletManager(){}
 
+    @Value("${api.data.serviceKey}")
+    private String serviceKey;
+
     @GetMapping("/subway")
     public String stationToiletList(Model model) {
 
-        String toiletList = "";
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "https://api.odcloud.kr/api/15044453/v1/uddi:4189de50-12db-4ae2-a9ca-dfb4d2e25101?page=1&perPage=1&serviceKey=" + serviceKey;
 
-        model.addAttribute("toiletList", toiletList);
+        ResponseEntity<Map> response = restTemplate.getForEntity(url, Map.class);
+        Map<String, Object> body = response.getBody();
+
+        List<Map<String, Object>> dataList = (List<Map<String, Object>>) body.get("data");
+
+        ObjectMapper mapper = new ObjectMapper();
+        List<ToiletDTO> allToilets = dataList.stream()
+                .map(m -> mapper.convertValue(m, ToiletDTO.class))
+                .collect(Collectors.toList());
+
+        List<String> stationNames = allToilets.stream()
+                .map(ToiletDTO::get역명)
+                .distinct()
+                .collect(Collectors.toList());
+
+        model.addAttribute("stations", stationNames);
 
         return "toilet/subway";
     }
 
-    @GetMapping("/subway-name")
+    @GetMapping("/station")
     public void stationNameToiletList() {}
 
-    @GetMapping("/toilet-name")
-    public void toiletDetails() {}
+//    @GetMapping("/station")
+//    public String toiletDetails(Model model, Map map) {
+//
+//        RestTemplate restTemplate = new RestTemplate();
+//        String url = "https://api.odcloud.kr/api/15044453/v1/uddi:4189de50-12db-4ae2-a9ca-dfb4d2e25101?page=1&perPage=1&serviceKey="+serviceKey;
+//
+//        // Map으로 response 받기
+//        ResponseEntity<Map> response = restTemplate.getForEntity(url, Map.class);
+//        Map<String, Object> body = response.getBody();
+//
+//        // data 배열 추출??
+//        List<Map<String, Object>> dataList = (List<Map<String, Object>>) body.get("data");
+//
+//        ObjectMapper mapper = new ObjectMapper();
+////        ToiletDTO toilet = mapper.convertValue(dataList.get(0), ToiletDTO.class);
+//        List<ToiletDTO> toilets = dataList.stream()
+//                        .map(m->mapper.convertValue(m, ToiletDTO.class))
+//                                .toList();
+//
+//        model.addAttribute("toilets", toilets);
+//
+//        return "toilet/toilet-name";
+//    }
 
 
 
