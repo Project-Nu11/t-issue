e10b7feef44b411e5976d81141b7dc4b245b734a
I7E-65\i7E-63 <rjsn4433@gmail.com>
2025-09-16 16:35:10 +0900

    [feature] 관리자 문의 페이지 구현
    관리자는 사용자의 문의를 문의 상태(대기중, 답변완료, 답변거절)에 따라 조회할 수 있으며 상세조회 버튼을 누르면 상세조회 모달창이 켜지고 답변 또는 답변 거절을 할 수 있다

--- src/main/java/com/toiletissue/request/model/dto/RequestDTO.java
+++ src/main/java/com/toiletissue/request/model/dto/RequestDTO.java
@@ -1,4 +1,117 @@
 package com.toiletissue.request.model.dto;
 
+import org.springframework.lang.NonNull;
+
 public class RequestDTO {
+
+    private int no;
+    private String id;
+    private String title;
+    private String content;
+    private String type;
+    private String datetime;
+    private String status;
+    private String answer;
+    private String reject;
+
+    public RequestDTO(){}
+
+    public RequestDTO(int no, String id, String title, String content, String type, String datetime, String status, String answer, String reject) {
+        this.no = no;
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.type = type;
+        this.datetime = datetime;
+        this.status = status;
+        this.answer = answer;
+        this.reject = reject;
+    }
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDatetime() {
+        return datetime;
+    }
+
+    public void setDatetime(String datetime) {
+        this.datetime = datetime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getReject() {
+        return reject;
+    }
+
+    public void setReject(String reject) {
+        this.reject = reject;
+    }
+
+    @Override
+    public String toString() {
+        return "RequestDTO{" +
+                "no=" + no +
+                ", id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", type='" + type + '\'' +
+                ", datetime='" + datetime + '\'' +
+                ", status='" + status + '\'' +
+                ", answer='" + answer + '\'' +
+                ", reject='" + reject + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/toiletissue/request/model/service/RequestService.java
+++ src/main/java/com/toiletissue/request/model/service/RequestService.java
@@ -1,4 +1,38 @@
 package com.toiletissue.request.model.service;
 
+import com.toiletissue.request.model.dao.RequestMapper;
+import com.toiletissue.request.model.dto.RequestDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
 public class RequestService {
+
+    @Autowired
+    private RequestMapper requestMapper;
+
+    public List<RequestDTO> selectAllRequest(String value) {
+        return requestMapper.selectAllRequest(value);
+    }
+
+    public void requestReject(RequestDTO requestDTO) {
+        requestMapper.requestReject(requestDTO);
+    }
+
+    public void requestAnswer(RequestDTO requestDTO) {
+        requestMapper.requestAnswer(requestDTO);
+    }
+
+    public List<RequestDTO> selectRequestsByCriteria() {
+        return requestMapper.selectRequestsByCriteria();
+    }
+
+    public List<RequestDTO> getListWithPaging(String value, int offset, int size) {
+        return requestMapper.selectPaging(value,offset,size);
+    }
+
+    public int getTotalRequest(String value) {
+        return requestMapper.getTotalRequest(value);
+    }
 }
