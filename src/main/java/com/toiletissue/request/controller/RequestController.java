f71e185413d765dc3e7be5307e57fe05c8cccc40
I7E-65\i7E-63 <rjsn4433@gmail.com>
2025-09-17 16:36:08 +0900

    [feature] 사용자 문의 페이지 구현
    사용자는 본인이 작성한 문의를 전체 및 상세조회할 수 있고 답변 상태에 따라 구분하여 조회할 수 있다.
    faq버튼을 눌러 faq 페이지로 이동하여 자주 묻는 질문을 확인할 수 있다.

--- src/main/java/com/toiletissue/request/controller/RequestController.java
+++ src/main/java/com/toiletissue/request/controller/RequestController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -20,7 +21,8 @@ public class RequestController {
     @GetMapping("/manager")
     public String requestManager(
             Model model,
-            @RequestParam(name="status",defaultValue = "%%") String value){
+            @RequestParam(name="status",defaultValue = "%%") String value,
+            @RequestParam(value = "page", defaultValue = "1") int page){
 
         List<RequestDTO> requestList = requestService.selectAllRequest(value);
 
@@ -29,30 +31,64 @@ public class RequestController {
         }
         System.out.println(value);
 
-        model.addAttribute("requestList",requestList);
-        model.addAttribute("value",value);
+        int totalRequests = requestList.size(); // 총 문의 수
+        int pageSize = 10; // 한 페이지당 문의 수
+        int totalPages = (int)(Math.ceil((double)totalRequests/pageSize)); // 총 페이지 수
 
+        if(page <= 0){
+            page = 1;
+        } else if(page > totalPages){
+            page = totalPages;
+        }
 
-        return "/request/manager";
-    }
+        int start = (page-1)*pageSize; //페이지 시작 문의 번호
+        int end = Math.min(start+pageSize,totalRequests); // 페이지 끝 문의 번호
+        List<RequestDTO> pagedRequests = requestList.subList(start,end);
 
-    @GetMapping("/manager/select")
-    public String requestSelect(Model model,@RequestParam(value = "value") String value){
-        List<RequestDTO> requestList = requestService.selectRequestsByCriteria();
-        for(RequestDTO req : requestList){
+        System.out.println("page = " + page);
+        System.out.println("totalPages = " + totalPages);
+        System.out.println("end = " + end);
+        System.out.println("start = " + start);
+        for(RequestDTO req : pagedRequests){
             System.out.println(req);
         }
-        System.out.println(value);
-        model.addAttribute("requestList",requestList);
+        int buttonCount = 10; // 한 번에 보여질 페이지 버튼 수
+        int startPage = ((page - 1) / buttonCount) * buttonCount + 1;
+        int endPage = Math.min(startPage + buttonCount - 1, totalPages);
+
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+
+
+
+
+        model.addAttribute("requestList",pagedRequests);
         model.addAttribute("value",value);
+        model.addAttribute("page",page);
+        model.addAttribute("totalPages",totalPages);
+
+
 
         return "/request/manager";
     }
+
+//    @GetMapping("/manager/select")
+//    public String requestSelect(Model model,@RequestParam(value = "value") String value){
+//        List<RequestDTO> requestList = requestService.selectRequestsByCriteria();
+//        for(RequestDTO req : requestList){
+//            System.out.println(req);
+//        }
+//        System.out.println(value);
+//        model.addAttribute("requestList",requestList);
+//        model.addAttribute("value",value);
+//
+//        return "/request/manager";
+//    }
 
     @PostMapping("/manager/reject")
     public ModelAndView requestReject(ModelAndView mv, @ModelAttribute RequestDTO requestDTO){
         requestService.requestReject(requestDTO);
-//        mv.setViewName("redirect:/request/manager");
+        mv.setViewName("redirect:/request/manager");
         return mv;
 
     }
@@ -71,6 +107,65 @@ public class RequestController {
 //
 //
 //    }
+
+    /*------------------------------------사용자 문의 기능------------------------------------------*/
+
+    @GetMapping("/member")
+    public String requestMember(
+            Model model,
+            @RequestParam(name="status",defaultValue = "%%") String value,
+            @RequestParam(value = "page", defaultValue = "1") int page){
+
+        List<RequestDTO> requestList = requestService.selectAllRequest(value);
+
+        for(RequestDTO req : requestList){
+            System.out.println(req);
+        }
+        System.out.println(value);
+
+        int totalRequests = requestList.size(); // 총 문의 수
+        int pageSize = 10; // 한 페이지당 문의 수
+        int totalPages = (int)(Math.ceil((double)totalRequests/pageSize)); // 총 페이지 수
+
+        if(page <= 0){
+            page = 1;
+        } else if(page > totalPages){
+            page = totalPages;
+        }
+
+        int start = (page-1)*pageSize; //페이지 시작 문의 번호
+        int end = Math.min(start+pageSize,totalRequests); // 페이지 끝 문의 번호
+        List<RequestDTO> pagedRequests = requestList.subList(start,end);
+
+        System.out.println("page = " + page);
+        System.out.println("totalPages = " + totalPages);
+        System.out.println("end = " + end);
+        System.out.println("start = " + start);
+        for(RequestDTO req : pagedRequests){
+            System.out.println(req);
+        }
+        int buttonCount = 10; // 한 번에 보여질 페이지 버튼 수
+        int startPage = ((page - 1) / buttonCount) * buttonCount + 1;
+        int endPage = Math.min(startPage + buttonCount - 1, totalPages);
+
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+
+
+
+
+        model.addAttribute("requestList",pagedRequests);
+        model.addAttribute("value",value);
+        model.addAttribute("page",page);
+        model.addAttribute("totalPages",totalPages);
+
+
+
+        return "/request/member";
+    }
+    /*-----------------FAQ------------------------*/
+    @GetMapping("/faq")
+    public void faq(){}
 
 
 }
